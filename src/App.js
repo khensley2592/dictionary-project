41c31bf2cd47ff4d50692b4e2440cc8c5631aeb0
khensley2592 <81331519+khensley2592@users.noreply.github.com>
2021-07-16 19:39:39 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">dic·tion·ar·y</header>
+    </div>
+  );
+}
+
+export default App;
