86a9eb3b523fbf0407bbdc08bfdf6ac49d974134
khensley2592 <81331519+khensley2592@users.noreply.github.com>
2021-07-16 20:16:50 -0400

    Added initial search engine

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
+import Dictionary from "./Dictionary.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">dic·tion·ar·y</header>
+      <div className="container">
+        <header className="App-header">dic·tion·ar·y</header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer>Coded by Katie Hensley</footer>
+      </div>
     </div>
   );
 }
