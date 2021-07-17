4ff29240869af4d871f916983cad121524d0b541
khensley2592 <81331519+khensley2592@users.noreply.github.com>
2021-07-16 20:55:20 -0400

    Added axios/API and API URL documentation

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,11 +1,17 @@
+import axios from "axios";
 import React, { useState } from "react";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+
+  function handleResponse(response) {}
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword} definition`);
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
